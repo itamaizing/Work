fa6442f9575d4ab2e37194da57cc48716e4faa71
Dmitry <platelen@gmail.com>
2024-03-27 12:50:14 +0300

    Fire_03
    Добавил плавное увеличение каста, при старте.

--- Assets/Scripts/Players/Abilities/Genjalf/Fireworks_Ability/Fireworks.cs
+++ Assets/Scripts/Players/Abilities/Genjalf/Fireworks_Ability/Fireworks.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Players.Abilities.Genjalf.Fireworks_Ability
 {
     public class Fireworks : MonoBehaviour
     {
         [SerializeField] private SoFireworksData _soFireworksData;
+        [SerializeField] private float _scaleChangeSpeed = 1.0f;
 
         private void Start()
         {
@@ -13,8 +15,29 @@ namespace Players.Abilities.Genjalf.Fireworks_Ability
 
         private void StartSetScaleFireworks()
         {
-            transform.localScale =
-                new Vector3(_soFireworksData.ScaleX, _soFireworksData.ScaleY, transform.localScale.z);
+            // Вычисляем конечный размер, используя данные из SoFireworksData
+            Vector3 targetScale = new Vector3(_soFireworksData.ScaleX, _soFireworksData.ScaleY, transform.localScale.z);
+            
+            // Запускаем корутину для плавного изменения масштаба
+            StartCoroutine(ScaleOverTime(targetScale));
+        }
+
+        private IEnumerator ScaleOverTime(Vector3 targetScale)
+        {
+            // Начальный размер объекта
+            Vector3 startScale = transform.localScale;
+
+            // Вычисляем расстояние до цели
+            float distance = Vector3.Distance(startScale, targetScale);
+            
+            while (Vector3.Distance(transform.localScale, targetScale) > 0.01f)
+            {
+                transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * _scaleChangeSpeed);
+                
+                yield return null;
+            }
+            
+            transform.localScale = targetScale;
         }
     }
 }
