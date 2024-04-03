8c030244db266accd611f7105d27dda32a332207
nokm4n <44049305+nokm4n@users.noreply.github.com>
2024-04-03 19:38:44 +0400

    Update RunePlayer.cs

--- Assets/Scripts/Players/RunePlayer.cs
+++ Assets/Scripts/Players/RunePlayer.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class RunePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private float _runeRegenerationDelay = 3;
+	[SerializeField] private float _runeRegenerationValue = 10;
+	//[SerializeField] private Image _runeBar;
+	private WaitForSeconds _waitForRegenRune;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public float rune;
+    public float regen;
+
+	private void Start()
+	{
+		UpdateRuneBar();
+		_waitForRegenRune = new WaitForSeconds(_runeRegenerationDelay);
+		StartCoroutine(RegenirateHP());
+	}
+
+	public void UpdateRuneBar()
+	{
+		
+	}
+	public void AddRune(float runeValue)
+	{
+		
+	}
+
+	public void RemoveRune(float runeValue) 
+	{
+
+	}
+	private IEnumerator RegenirateHP()
+	{
+		while (true)
+		{
+			yield return _waitForRegenRune;
+			this.AddRune(_runeRegenerationValue);
+		}
+	}
 }
