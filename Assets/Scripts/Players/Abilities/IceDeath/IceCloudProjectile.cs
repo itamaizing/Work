98a53c377fc7b6764c2b7a69f3aa6028cac9caa2
nokm4n <44049305+nokm4n@users.noreply.github.com>
2024-05-02 17:45:56 +0400

    Update IceCloudProjectile.cs

--- Assets/Scripts/Players/Abilities/IceDeath/IceCloudProjectile.cs
+++ Assets/Scripts/Players/Abilities/IceDeath/IceCloudProjectile.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class IceCloudProjectile : MonoBehaviour
@@ -30,7 +32,7 @@ public class IceCloudProjectile : MonoBehaviour
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		if (collision.gameObject == dad)
+		if (collision.gameObject == dad || collision.CompareTag("Ability"))
 			return;
 		//damage, freez etc
 		if(collision.TryGetComponent<CharacterState>(out var target))
