cdcd1afce48073583c6db799ee23672c84a4d2f4
H3k70 <ser0307z@gmail.com>
2024-05-11 01:04:41 +0300

    Смена цвета радиуса

--- Assets/Scripts/Players/Abilities/AbilityRender.cs
+++ Assets/Scripts/Players/Abilities/AbilityRender.cs
@@ -9,6 +9,7 @@ public class AbilityRender : MonoBehaviour
     [SerializeField] private SpriteRenderer _areaPref;
     [SerializeField] private SpriteRenderer _squareAreaPref;
 
+    private float _radius;
     private SpriteRenderer _icon;
     private SpriteRenderer _area;
     private SpriteRenderer _squareArea;
@@ -26,6 +27,9 @@ public class AbilityRender : MonoBehaviour
         _squareArea = Instantiate(_squareAreaPref, transform);
         _squareArea.size = new Vector2(ability.CastWidth, ability.CastLength);
 
+        _radius = ability.Radius;
+        _circle.Draw(_radius);
+
         _drawCursorAbilityIconJob = StartCoroutine(DrawCoroutine());
     }
 
@@ -40,10 +44,22 @@ public class AbilityRender : MonoBehaviour
         Destroy(_icon.gameObject);
         Destroy(_area.gameObject);
         Destroy(_squareArea);
+        _circle.Clear();
 
         _icon = null;
         _area = null;
         _squareArea = null;
+        _radius = 0;
+    }
+
+    private bool IsMouseInRadius(float radius)
+    {
+        float distance = Vector3.Distance(
+            new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, transform.position.z),
+            transform.position
+            );
+
+        return distance <= radius;
     }
 
     private void RotateAtMouse(Transform transform)
@@ -61,6 +77,11 @@ public class AbilityRender : MonoBehaviour
             _icon.transform.position = mouse;
             _area.transform.position = mouse;
             RotateAtMouse(_squareArea.transform);
+
+            if (IsMouseInRadius(_radius))
+                _circle.SetColor(Color.green);
+            else
+                _circle.SetColor(Color.red);
 
             yield return null;
         }
