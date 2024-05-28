985fc0397593d402d60daaacae3990a8e8660de1
H3k70 <ser0307z@gmail.com>
2024-05-28 20:47:41 +0300

    добавлены сцены

---
+++ Assets/Scripts/Multiplayer/MultiplayerManager.cs
@@ -0,0 +1,29 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplayerManager : NetworkManager
+{
+    [SerializeField] private UserPrefab _userPrefab;
+    [SerializeField, Scene] private string _onlineScene;
+
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        if (Utils.IsSceneActive(_onlineScene))
+        {
+            var player = Instantiate(_userPrefab);
+            NetworkServer.AddPlayerForConnection(conn, player.gameObject);
+        }
+    }
+
+    public override void OnStartClient()
+    {
+        GameObject[] prefabs = Resources.LoadAll<GameObject>("MPPrefabs");
+
+        for (int i = 0; i < prefabs.Length; i++)
+        {
+            NetworkClient.RegisterPrefab(prefabs[i]);
+        }
+    }
+}
