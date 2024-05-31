68c0383b32c5ed102094073f70e8f8433ca629cc
h3k70 <ser0307z@gmail.com>
2024-05-31 21:44:33 +0300

    Add class TargetAbility

---
+++ Assets/Scripts/Players/Abilities/TargetAbility.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetAbility : Ability
+{
+    protected GameObject _target;
+
+    protected override void Cancel()
+    {
+        
+    }
+
+    protected override void Cast()
+    {
+        
+    }
+}
