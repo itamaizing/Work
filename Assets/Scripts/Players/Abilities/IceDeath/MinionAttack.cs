f1ea93ae8114e3e26cb6e955be12da078d40c701
nokm4n <44049305+nokm4n@users.noreply.github.com>
2024-06-05 16:50:53 +0400

    Minion attack and base for Empathic field

--- Assets/Scripts/Players/Abilities/IceDeath/MinionAttack.cs
+++ Assets/Scripts/Players/Abilities/IceDeath/MinionAttack.cs
@@ -11,25 +11,16 @@ using static UnityEngine.GraphicsBuffer;
 
 public class MinionAttack : Ability
 {
-	[SerializeField] private float _damage = 8f;
+	[SerializeField] private float _damage = 3f;
 	[SerializeField] private Character _dad;
-	[SerializeField] private float _abilityCooldown = 1.4f; //cooldown between shots
+	[SerializeField] private float _abilityCooldown = 1.6f; //cooldown between shots
 	[SerializeField] private LayerMask _obstacleLayerMask;
-	private float _cooldownTimer = 1.4f;
-	private int _hitInARow = 0;
-	private float _multiplySpeed = .05f;
-	private bool _isInTheRow = false;
-	private float _baseTimer = 2f; //time and timer between losing streak
-	private float _timer = 2f;
+	private float _cooldownTimer = 1.6f;
 	private bool _isReadyToShot = true;
-	private Character _target;
-	private Vector2 _jumpPos;
-
-	public Character Target => _target;
-	public int HitInTheRow => _hitInARow;
 
 	private void Update()
 	{
+		if (_isReadyToShot) return;
 		Timer();
 	}
 	protected override void Cancel()
@@ -64,52 +55,10 @@ public class MinionAttack : Ability
 			}			
 		}
 	}
-
 	private void Hit(Character enemy)
 	{
 		_isReadyToShot = false;
-		if(_target == enemy && _dad.Stamina.Use(5))
-		{
-			//Debug.Log("hit " + _hitInARow);
-			_hitInARow++;
-			_multiplySpeed*=2;
-			_timer = _baseTimer;
-			_isInTheRow = true;
-
-			enemy.Health.TakeDamage(_damage + Random.Range(0, 2), DamageType.Physical);
-			if (_hitInARow >= 6)
-			{
-				//Debug.Log("Lasthit");
-				LastHit();
-			}
-		}
-		else
-		{
-			//Debug.Log("lose streak to another enemy");
-			_target = enemy;
-			_hitInARow = 0;
-			_multiplySpeed = .05f;
-			_timer = _baseTimer;
-			_isInTheRow = true;
-			
-			enemy.Health.TakeDamage(_damage + Random.Range(0, 2), DamageType.Physical);
-		}
-	}
-	private void LastHit()
-	{
-		if (_dad.Stamina.Use(10))
-		{
-			_target.Health.TakeDamage(_damage * .5f, DamageType.Physical);
-			_target.CharacterState.AddState(new StunnedState(), 1.5f, 0, States.Stun);
-			PushBackEnemy(_target);
-			//отбрасывание 			
-		}
-		_dad.Stamina.Add(_dad.Stamina.MaxValue*0.4f);
-		_hitInARow = 0;
-		_target = null;
-		_isInTheRow= false;
-		_multiplySpeed = 0.05f;
-		_timer = _baseTimer;
+		enemy.Health.TakeDamage(_damage + Random.Range(0, 1), DamageType.Physical);
 	}
 
 	public void Timer()
@@ -121,84 +70,7 @@ public class MinionAttack : Ability
 		else
 		{
 			_isReadyToShot = true;
-			_cooldownTimer = _abilityCooldown * (1 - _multiplySpeed);
+			_cooldownTimer = _abilityCooldown;
 		}
-		if (_isInTheRow)
-		{
-			_timer -= Time.deltaTime;
-			if (_timer <= 0)
-			{
-				_target = null;
-				_multiplySpeed = 0.05f;
-				Debug.Log("lose streak");
-				_timer = _baseTimer;
-				_isInTheRow = false;
-				_hitInARow = 0;
-			}
-		}
-	}
-
-	private void PushBackEnemy(Character enemy)
-	{
-		Debug.Log("Push");
-		Vector2 pushPos = (_dad.Rb.position - enemy.Rb.position).normalized;
-		Vector2 endPos = -pushPos * 2;
-		//enemy.PlayerMove.CanMove = false;
-		//Debug.DrawLine(enemy.Rb.position, enemy.Rb.position + endPos * 10, Color.red, Mathf.Infinity);
-		if (CheckObstacleBetween(enemy.Rb.position, endPos))
-		{
-			enemy.Rb.DOMove(_jumpPos, 1).SetEase(Ease.Linear);
-		}
-		else
-		{
-			enemy.Rb.DOMove(enemy.Rb.position + endPos, 1).SetEase(Ease.Linear);
-		}
-	}
-
-	private bool CheckObstacleBetween(Vector3 start, Vector3 end)
-	{
-		//Проверка на наличие препятствия
-		Vector2 direction = (end - start).normalized;
-		float distance = Vector2.Distance(start, end);
-
-		RaycastHit2D[] hits =
-			Physics2D.BoxCastAll(start, new Vector2(1f, 1f), 0f, direction, distance, _obstacleLayerMask);
-
-		foreach (RaycastHit2D hit in hits)
-		{
-			Debug.Log(hit.collider.gameObject.name);
-			_jumpPos = hits[0].point - direction;
-			return true;
-		}
-
-		return false;
-	}
-
-	public void HitFromSword(int hitInTheRow, float multiplySpeed)
-	{
-		_hitInARow = hitInTheRow;
-		_multiplySpeed = multiplySpeed;
-		_timer = _baseTimer;
-		_isInTheRow = true;
-	}
-
-	public void HitFromSword(Character enemy)
-	{
-		Debug.Log("hit from sword");
-		_target = enemy;
-		_hitInARow++;
-		_multiplySpeed *= 2;
-		_timer = _baseTimer;
-		_isInTheRow = true;
-	}
-
-	public void LoseStreak()
-	{
-		_target = null;
-		_multiplySpeed = 0.05f;
-		Debug.Log("lose streak");
-		_timer = _baseTimer;
-		_isInTheRow = false;
-		_hitInARow = 0;
 	}
 }
