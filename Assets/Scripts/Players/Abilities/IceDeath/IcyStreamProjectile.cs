a5903b9a232e69631ed4800b477b7f545e699e78
nokm4n <44049305+nokm4n@users.noreply.github.com>
2024-06-17 19:01:41 +0400

    Icy Stream added

---
+++ Assets/Scripts/Players/Abilities/IceDeath/IcyStreamProjectile.cs
@@ -0,0 +1,110 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class IcyStreamProjectile : MonoBehaviour
+{
+	public float energyDad;
+	[HideInInspector] public Character dad;
+
+	[SerializeField] private Rigidbody2D _rb;
+	[SerializeField] GameObject _hitEffect;
+	[SerializeField] SpriteRenderer _spriteRenderer;
+	[SerializeField] private float _force;
+	[SerializeField] private float _distance = 4;
+
+	private float _timer = 1.5f;
+	private float _time = 0;
+	private float _stepOfTimer = 0.3f;
+	private float _damage = 1;
+	private float _chanceOfFrosting = 0.05f;
+	private float _durationOfDebuff = 2;
+	private List<Character> _enemyList;
+	//private Vector2 startPos;
+
+	private void Awake()
+	{
+		Debug.Log("bullet");
+		//startPos = transform.position;
+		//_rb.AddForce(transform.up * _force, ForceMode2D.Impulse);
+		if (energyDad >= 40)
+		{
+			_durationOfDebuff += 4;
+		}
+		else
+		{
+			_durationOfDebuff += energyDad / 10;
+		}
+		_rb.DOMove(transform.up * _distance * GlobalVariable.cellSize, _timer).OnComplete(Explode);
+	}
+
+	private void Update()
+	{
+		Timer();
+		_spriteRenderer.DOFade(0, _timer);
+		/*if (Vector2.Distance(transform.position, startPos) > _distance * GlobalVariable.cellSize)
+		{
+			Explode();
+		}*/
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject == dad.gameObject || collision.CompareTag("Ability"))
+			return;
+
+		if (collision.TryGetComponent<Character>(out var target))
+		{
+			_enemyList.Add(target);
+			Energy energyLink = (Energy)dad.Stamina;
+			energyLink.SumDamageMake(_damage);
+			target.Health.TryTakeDamage(_damage, DamageType.Magical, AttackRangeType.RangeAttack);
+			target.CharacterState.AddState(new Cooling(), _durationOfDebuff, 0, States.Cooling);
+		}
+		//Explode();
+	}
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject == dad.gameObject || collision.CompareTag("Ability"))
+			return;
+
+		if (collision.TryGetComponent<Character>(out var target))
+		{
+			_enemyList.Remove(target);
+		}
+	}
+
+	private void Explode()
+	{
+		_enemyList.Clear();
+		if (_hitEffect != null)
+		{
+			GameObject hitEffect = Instantiate(_hitEffect, transform.position, Quaternion.identity);
+			Destroy(hitEffect, 5f);
+		}
+		Destroy(gameObject);
+	}
+
+	private void Timer()
+	{
+		_time += Time.deltaTime;
+		if(_time >= _stepOfTimer)
+		{
+			_time = 0;
+			_distance++;
+			_chanceOfFrosting *= 2;
+			foreach(var enemy in _enemyList) 
+			{
+				enemy.Health.TryTakeDamage(_damage, DamageType.Magical, AttackRangeType.RangeAttack);
+
+				enemy.CharacterState.AddState(new Cooling(), _durationOfDebuff, 0, States.Cooling);
+				if (Random.Range(0, 1f) <= _chanceOfFrosting)
+				{
+					enemy.CharacterState.AddState(new FrostingState(), _durationOfDebuff, 0, States.Frosting);
+				}
+			}
+		}
+	}
+}
