e3a5ef37e2a58d6e81ce804b4484410019375b55
nokm4n <44049305+nokm4n@users.noreply.github.com>
2024-07-05 16:29:33 +0400

    New fog

--- Assets/FogOfWar/Scripts/VisionComponent.cs
+++ Assets/FogOfWar/Scripts/VisionComponent.cs
@@ -18,7 +18,6 @@ public class VisionComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         m_Vision.SetPosition(new Vector3(transform.position.x, transform.position.z, transform.position.y));
         m_Vision.SetRange(m_VisionRange);
     }
--- Assets/Scripts/Prefabs/USABLE/TalentSystem.cs
+++ Assets/Scripts/Prefabs/USABLE/TalentSystem.cs
@@ -110,12 +110,12 @@ public class TalentSystem : NetworkBehaviour
     }
 
     [ClientRpc]
-    private void RpcAdd(Talent talent)
+    public void RpcAdd(Talent talent)
     {
         Add(talent);
     }
 
-    private void Add(int id)
+	public void Add(int id)
     {
 		Debug.Log("Add");
 		if (_talents.Count >= id && _points > _activeTalents.Count)
@@ -126,7 +126,7 @@ public class TalentSystem : NetworkBehaviour
         }
     }
 
-    private void Remove(int id) 
+	public void Remove(int id) 
     {
 		Debug.Log("Removes");
 		if (_talents.Count >= id)
@@ -137,7 +137,7 @@ public class TalentSystem : NetworkBehaviour
 		}
 	}
 
-    private void EnterAll()
+	public void EnterAll()
     {
         foreach(Talent talent in _activeTalents)
         {
@@ -145,7 +145,7 @@ public class TalentSystem : NetworkBehaviour
         }
     }
 
-    private void ExitAll()
+	public void ExitAll()
     {
 		foreach (Talent talent in _activeTalents)
 		{
@@ -154,13 +154,13 @@ public class TalentSystem : NetworkBehaviour
 		}
 	}
 
-    private void Add(Talent talent)
+	public void Add(Talent talent)
     {
         _activeTalents.Add(talent);
         talent.Enter();
     }
 
-    private void Remove(Talent talent)
+	public void Remove(Talent talent)
     {
         talent.Exit();
         _activeTalents.Remove(talent);
