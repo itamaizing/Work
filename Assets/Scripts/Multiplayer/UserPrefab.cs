c35f1985f8f9785b6f68c7829f3588f247a2a020
h3k70 <ser0307z@gmail.com>
2024-07-06 22:18:33 +0300

    preparation

--- Assets/Scripts/Multiplayer/UserPrefab.cs
+++ Assets/Scripts/Multiplayer/UserPrefab.cs
@@ -1,8 +1,30 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UserPrefab : MonoBehaviour
+public class UserPrefab : NetworkBehaviour
 {
+    [SyncVar]
+    public int playerNumber;
 
+    [SyncVar]
+    public int scoreIndex;
+
+    [SyncVar]
+    public int matchIndex;
+
+    [SyncVar]
+    public uint score;
+
+    public int clientMatchIndex = -1;
+
+    void OnGUI()
+    {
+        if (!isServerOnly && !isLocalPlayer && clientMatchIndex < 0)
+            clientMatchIndex = NetworkClient.connection.identity.GetComponent<UserPrefab>().matchIndex;
+
+        if (isLocalPlayer || matchIndex == clientMatchIndex)
+            GUI.Box(new Rect(10f + (scoreIndex * 110), 10f, 100f, 25f), $"P{playerNumber}: {score}");
+    }
 }
