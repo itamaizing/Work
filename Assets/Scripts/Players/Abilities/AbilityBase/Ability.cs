0cf28f888db60fd69dec402a9282b44eaa8a866e
h3k70 <ser0307z@gmail.com>
2024-07-11 14:08:24 +0300

    fix

--- Assets/Scripts/Players/Abilities/AbilityBase/Ability.cs
+++ Assets/Scripts/Players/Abilities/AbilityBase/Ability.cs
@@ -74,8 +74,7 @@ public abstract class Ability : NetworkBehaviour
     public bool IsUsed { get => _isUsed; protected set => _isUsed = value; }
     public bool IsCanCancle { get => _isCanCancle; protected set => _isCanCancle = value; }
     public bool IsReady { get => _isReady; set => _isReady = value; }
-    public float RemainingСooldownTime => _remainingСooldownTime;
-    public Schools School => _abilitySchool;
+	public Schools School => _abilitySchool;
 	public AbilityForm AbilityForm => _abilityForm;
     public StatsBuff Buff => _statsBuff;
 
