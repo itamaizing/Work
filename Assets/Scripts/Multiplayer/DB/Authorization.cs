92d9c2dbcecd1678316857ea4e3a60a6ba8700be
h3k70 <ser0307z@gmail.com>
2024-08-06 13:02:33 +0300

    Authorization

---
+++ Assets/Scripts/Multiplayer/DB/Authorization.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Authorization : MonoBehaviour
+{
+    private const string LOGIN = "login";
+    private const string PASSWORD = "password";
+
+    private int _id;
+    private string _login;
+    private string _password;
+
+    public void SetLogin(string login)
+    {
+        _login = login;
+    }
+
+    public void SetPassword(string password)
+    {
+        _password = password;
+    }
+
+    public void SignIn()
+    {
+        if(string.IsNullOrEmpty(_login) || string.IsNullOrEmpty(_password))
+        {
+            Debug.LogError("login or pass not validate");
+            return;
+        }
+        Dictionary<string, string> data = new Dictionary<string, string>()
+        {
+            {LOGIN, _login},
+            {PASSWORD, _password }
+        };
+
+        NetworkHTTP.Instance.Post(URLLibrary.Authorization, data, Success);
+    }
+
+    private void Success(string data)
+    {
+        if(int.TryParse(data, out int id))
+        {
+            _id = id;
+        }
+    }
+}
