a4627491aad1aa77be7bd5150cc60edd17c4a736
h3k70 <ser0307z@gmail.com>
2024-08-06 14:47:59 +0300

    registration

--- Assets/Scripts/Multiplayer/DB/AuthorizationUI.cs
+++ Assets/Scripts/Multiplayer/DB/AuthorizationUI.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AuthorizationUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Authorization _authorization;
+    [SerializeField] private InputField _login;
+    [SerializeField] private InputField _password;
+    [SerializeField] private Button _signIn;
+    [SerializeField] private Button _signUp;
+    [SerializeField] private TMP_Text _error;
+
+    private void Awake()
     {
-        
+        _login.onEndEdit.AddListener(_authorization.SetLogin);
+        _password.onEndEdit.AddListener(_authorization.SetPassword);
+
+        _signIn.onClick.AddListener(OnSignIn);
+
+        _authorization.Error += OnError;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSignIn()
     {
-        
+        _signIn.interactable = false;
+        _signUp.interactable = false;
+        _authorization.SignIn();
+    }
+
+    private void OnError(string data)
+    {
+        _signIn.interactable = true;
+        _signUp.interactable = true;
+        _error.text = data;
     }
 }
--- Assets/Scripts/Multiplayer/DB/URLLibrary.cs
+++ Assets/Scripts/Multiplayer/DB/URLLibrary.cs
@@ -6,6 +6,8 @@ public class URLLibrary
 {
     private const string _main = "http://147.45.141.9/";
     private const string _authorization = "ProjectGame/authorization.php";
+    private const string _registration = "ProjectGame/registration.php";
 
     public static string Authorization { get { return _main + _authorization; } }
+    public static string Registration { get { return _main + _registration; } }
 }
