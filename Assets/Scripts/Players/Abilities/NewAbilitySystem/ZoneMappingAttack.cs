d0dad2e41d69f5964802a3ebc69bf876be0ddbfd
BrednikFiryn <FirynBrednik@mail.ru>
2024-09-18 09:46:45 +0500

    Working with SkillRenderer by teams

---
+++ Assets/Scripts/Players/Abilities/NewAbilitySystem/ZoneMappingAttack.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneMappingAttack : Skill
+{
+    private Vector3 _targetPoint = Vector3.positiveInfinity;
+
+    protected override bool IsCanCast => true;
+
+    protected override IEnumerator PrepareJob()
+    {
+        while (float.IsPositiveInfinity(_targetPoint.x))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 clickedPoint = GetMousePoint();
+
+                if (IsPointInRadius(Radius, clickedPoint))
+                {
+                    _targetPoint = clickedPoint;
+                }
+            }
+            yield return null;
+        }
+    }
+
+    protected override IEnumerator CastJob()
+    {
+        DrawDamageZone(_targetPoint);
+
+        yield return new WaitForSeconds(2f);
+        StopDamageZone();
+    }
+
+    protected override void ClearData()
+    {
+        _targetPoint = Vector3.positiveInfinity;
+    }
+}
+
