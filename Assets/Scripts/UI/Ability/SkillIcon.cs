583de015637752b460ec4deea1f10d13f18e6450
h3k70 <ser0307z@gmail.com>
2024-09-21 00:02:41 +0300

    The logic of the ability icons has been changed

--- Assets/Scripts/UI/Ability/SkillIcon.cs
+++ Assets/Scripts/UI/Ability/SkillIcon.cs
@@ -9,14 +9,9 @@ using UnityEngine.UI;
 public class SkillIcon : MonoBehaviour, IDropHandler
 {
     [SerializeField] private Image _boxFrame;
-    [SerializeField] private Blink _blinkBoxFrame;
     [SerializeField] private TMP_Text _key;
 
-    [SerializeField] private FillAmountOverTime _cooldown;
-    [SerializeField] private TextMeshProUGUI _chargeCounter;
-
     private int _index;
-    private FillAmountOverTime _castLine;
     private DraggableIcon _currentIcon;
 
     public event Action<int, Skill> CurrentSkillChenged;
@@ -28,9 +23,6 @@ public class SkillIcon : MonoBehaviour, IDropHandler
         {
             if (value == null)
             {
-                if(_currentIcon != null)
-                    UnsubscribingSkillOnEvents(_currentIcon.Skill);
-
                 _currentIcon = value;
 
                 Deselected();
@@ -45,31 +37,25 @@ public class SkillIcon : MonoBehaviour, IDropHandler
                     Selected();
                 else
                     Deselected();
-                SubscribingSkillOnEvents(_currentIcon.Skill);
             }
             else
             {
-                UnsubscribingSkillOnEvents(_currentIcon.Skill);
-
                 _currentIcon = value;
                 if (_currentIcon.Selected)
                     Selected();
                 else
                     Deselected();
-                SubscribingSkillOnEvents(_currentIcon.Skill);
             }
             CurrentSkillChenged?.Invoke(_index, _currentIcon.Skill);
         }
     }
 
     public TMP_Text Key { get => _key; }
-    public FillAmountOverTime CastLine { get => _castLine; set => _castLine = value; }
     public int Index { get => _index; }
 
-    public void Init(int index, FillAmountOverTime castLine)
+    public void Init(int index)
     {
         _index = index;
-        _castLine = castLine;
     }
 
     public void OnDrop(PointerEventData eventData)
@@ -89,12 +75,6 @@ public class SkillIcon : MonoBehaviour, IDropHandler
             draggableIcon.PatentAfterDrag = transform;
             CurrentIcon = draggableIcon;
         }
-    }
-
-    public void OnStartCooldown(float dutarion)
-    {
-        _cooldown.gameObject.SetActive(true);
-        _cooldown.StartFill(dutarion, 1, 0, false);
     }
 
     public void Selected()
@@ -122,83 +102,5 @@ public class SkillIcon : MonoBehaviour, IDropHandler
     public void Hide()
     {
         gameObject.SetActive(false);
-    }
-
-    private void SubscribingSkillOnEvents(Skill ability)
-    {
-        //ability.CastStreamStarted += OnStartStreaming;
-        //ability.Canceled += OnStopStreaming;
-
-        //ability.CastDeleyStarted += OnStartCastDeley;
-        //ability.Canceled += OnStopCastDeley;
-
-        ability.CooldownStarted += OnStartCooldown;
-        ability.CurrentChargeChanged += OnCurrentChargeText;
-
-        ability.CastStarted += OnCastStarted;
-        ability.CastEnded += OnCastEnded;
-        ability.Canceled += OnCastEnded;
-    }
-
-    private void UnsubscribingSkillOnEvents(Skill ability)
-    {
-        //ability.CastStreamStarted -= OnStartStreaming;
-        //ability.Canceled -= OnStopStreaming;
-
-        //ability.CastDeleyStarted -= OnStartCastDeley;
-        //ability.Canceled -= OnStopCastDeley;
-
-        ability.CooldownStarted -= OnStartCooldown;
-        ability.CurrentChargeChanged -= OnCurrentChargeText;
-
-        ability.CastStarted -= OnCastStarted;
-        ability.CastEnded -= OnCastEnded;
-        ability.Canceled -= OnCastEnded;
-    }
-
-    private void OnCastStarted()
-    {
-        _blinkBoxFrame.gameObject.SetActive(true);
-        _blinkBoxFrame.StartBlink(0.5f);
-    } 
-
-    private void OnCastEnded()
-    {
-        _blinkBoxFrame.StopBlink();
-        _blinkBoxFrame.gameObject.SetActive(false);
-    }
-
-    private void OnCurrentChargeText(int value)
-    {
-        if (value > 0)
-            _chargeCounter.color = Color.green;
-        else
-            _chargeCounter.color = Color.red;
-
-        _chargeCounter.text = value.ToString();
-    }
-
-    private void OnStartStreaming(float time)
-    {
-        _castLine.gameObject.SetActive(true);
-        _castLine.StartFill(time, 1, 0);
-    }
-
-    private void OnStopStreaming()
-    {
-        _castLine.gameObject.SetActive(false);
-        _castLine.Stop();
-    }
-
-    private void OnStartCastDeley(float time)
-    {
-        _castLine.gameObject.SetActive(true);
-        _castLine.StartFill(time);
-    }
-
-    private void OnStopCastDeley()
-    {
-        _castLine.gameObject.SetActive(false);
-        _castLine.Stop();
     }
 }
