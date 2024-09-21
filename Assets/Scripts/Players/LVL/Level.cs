397e3e9be4577c88f1e072940400397eaeac613c
h3k70 <ser0307z@gmail.com>
2024-09-21 19:29:07 +0300

    Add LVL

---
+++ Assets/Scripts/Players/LVL/Level.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    protected int _experienceForNextLVL = 100;
+    protected int _additionalToExperienceForNextLVL = 10;
+    protected float _multiplierToExperienceForNextLVL = 1;
+    private int _experience = 0;
+    private int _value = 1;
+
+    public int Value { get => _value; protected set { _value = value; LVLUped?.Invoke(_value); } }
+    public int Experience { get => _experience; }
+    public int ExperienceForNextLVL { get => _experienceForNextLVL; }
+
+    public event Action<int> EXPAdded;
+    public event Action<int> LVLUped;
+
+    public void AddEXP(int value)
+    {
+        if (value <= 0)
+            return;
+
+        _experience += value;
+        EXPAdded?.Invoke(value);
+
+        var expBeyondNecessery = _experience - _experienceForNextLVL;
+
+        if (expBeyondNecessery >= 0)
+        {
+            _value++;
+            LVLUped?.Invoke(_value);
+
+            _experience = 0;
+            IncreasExperienceForNextLVL();
+            AddEXP(expBeyondNecessery);
+        }
+    }
+
+    private void IncreasExperienceForNextLVL()
+    {
+        var temp = _experienceForNextLVL * _multiplierToExperienceForNextLVL;
+        _experienceForNextLVL = (int)temp + _additionalToExperienceForNextLVL;
+    }
+}
