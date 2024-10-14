199df333d41be1662baecdd1674d1c6e45c3b97d
h3k70 <ser0307z@gmail.com>
2024-10-15 00:54:00 +0300

    0.8 minionCamp

--- Assets/Scripts/Prefabs/USABLE/HeroComponent.cs
+++ Assets/Scripts/Prefabs/USABLE/HeroComponent.cs
@@ -4,9 +4,7 @@ using UnityEngine.Serialization;
 public class HeroComponent : Character
 {
     [FormerlySerializedAs("talents")] [SerializeField] private TalentSystem talentManager;
-    [SerializeField] private SpawnComponent _spawnComponent;
 
-    public SpawnComponent SpawnComponent => _spawnComponent;
     public TalentSystem TalentManager => talentManager;
 
     public override void Initialize()
