28f61480641d81cdf857f23e188d48c5098b429c
h3k70 <ser0307z@gmail.com>
2025-06-17 22:11:25 +0300

    Add UIParticle

--- Assets/Scripts/UI/Ability/AutoCastParticles.cs
+++ Assets/Scripts/UI/Ability/AutoCastParticles.cs
@@ -2,24 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Splines;
+using DG.Tweening;
 
 public class AutoCastParticles : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem _particle1;
-    [SerializeField] private ParticleSystem _particle2;
+    //[SerializeField] private ParticleSystem _particle1;
+    //[SerializeField] private ParticleSystem _particle2;
+    [SerializeField] private float _duration = 3;
+    [SerializeField] private Transform[] _transformpath1;
+    [SerializeField] private Transform[] _transformpath2;
+    [SerializeField] private GameObject _particle1;
+    [SerializeField] private GameObject _particle2;
+
+    private Vector3[] _path1;
+    private Vector3[] _path2;
+
     private void Start()
     {
         gameObject.SetActive(false);
+
     }
 
     public void Play()
     {
-        //_particle1.Play();
-        //_particle2.Play();
+        _path1 = new Vector3[(_transformpath1.Length)];
+        _path2 = new Vector3[(_transformpath2.Length)];
+        _particle1.transform.position = _transformpath1[0].position;
+        _particle2.transform.position = _transformpath2[0].position;
 
-        //_particle1.GetComponent<SplineAnimate>().Play();
-        //_particle2.GetComponent<SplineAnimate>().Play();
+        for (int i = 0; i < _path1.Length; i++)
+            _path1[i] = transform.InverseTransformPoint(_transformpath1[i].position);
 
+        for (int i = 0; i < _path2.Length; i++)
+            _path2[i] = transform.InverseTransformPoint(_transformpath2[i].position);
+        /*
         _particle1.gameObject.SetActive(false);
         _particle2.gameObject.SetActive(false);
 
@@ -27,5 +43,19 @@ public class AutoCastParticles : MonoBehaviour
         _particle2.gameObject.SetActive(true);
 
         _particle2.GetComponent<SplineAnimate>().StartOffset = 0.51f;
+        */
+        _particle1.transform.localPosition = _path1[0];
+        _particle1.transform.DOLocalPath(_path1, _duration).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+        _particle2.transform.localPosition = _path2[0];
+        _particle2.transform.DOLocalPath(_path2, _duration).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+    }
+
+    public void Stop()
+    {
+        _particle1.transform.DOKill();
+        _particle2.transform.DOKill();
+
+        _particle1.transform.position = _transformpath1[0].position;
+        _particle2.transform.position = _transformpath2[0].position;
     }
 }
