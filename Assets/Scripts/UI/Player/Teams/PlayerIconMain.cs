c96a6b66f39bffefe66767acded086fb2875aa1d
h3k70 <ser0307z@gmail.com>
2025-06-20 23:58:55 +0300

    add frame icon blink

--- Assets/Scripts/UI/Player/Teams/PlayerIconMain.cs
+++ Assets/Scripts/UI/Player/Teams/PlayerIconMain.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class PlayerIconMain : PlayerIcon
+public class PlayerIconMain : PlayerIcon, IPointerExitHandler, IPointerEnterHandler
 {
     [SerializeField] private LvlInfo _lvlInfo;
+    [SerializeField] private Image _FrameBlink;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _FrameBlink.gameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _FrameBlink.gameObject.SetActive(false);
+    }
 
     protected override void UpdateInfo(Character character)
     {
