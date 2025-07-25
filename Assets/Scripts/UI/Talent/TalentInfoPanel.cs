2ebdd78f5f8dc5bd657f24e9223dd2d7ffe8772c
BrednikFiryn <FirynBrednik@mail.ru>
2025-07-26 02:06:22 +0500

    Brednik26072025_1

--- Assets/Scripts/UI/Talent/TalentInfoPanel.cs
+++ Assets/Scripts/UI/Talent/TalentInfoPanel.cs
@@ -13,41 +13,47 @@ public class TalentInfoPanel : MonoBehaviour
     public void Show(TalentData data)
     {
         gameObject.SetActive(true);
-        bool hasStateInfos = data.StateInfos.Count > 0;
-
-        foreach (var text in data.DescriptionsForInfoPanel)
-        {
-            var cell = Instantiate(_cellPref, transform);
-            _cells.Add(cell);
-
-            cell.ShowDividingLine();
-            cell.TextDescription.text = text;
-        }
-
-        if (!hasStateInfos && _cells.Count > 0) _cells[^1].HideDividingLine();
-
-        foreach (var st in data.StateInfos)
-        {
-            var cell = Instantiate(_cellPref, transform);
-            _cells.Add(cell);
-
-            cell.HideDividingLine();
-            cell.TextDescription.text = $"<color=#FFFF00>{st.StateName}</color> - {st.Description}";
-        }
-
         _name.text = data.Description;
+
+        int descCnt = data.DescriptionsForInfoPanel.Count;
+        int stateCnt = data.StateInfos.Count;
+        int pairCnt = Mathf.Max(descCnt, stateCnt);
+
+        for (int i = 0; i < pairCnt; i++)
+        {
+            if (i < descCnt)
+            {
+                var descCell = Instantiate(_cellPref, transform);
+                _cells.Add(descCell);
+
+                descCell.TextDescription.text = data.DescriptionsForInfoPanel[i];
+
+                if (i == 0) descCell.HideDividingLine();
+                else descCell.ShowDividingLine();
+            }
+
+            if (i < stateCnt)
+            {
+                var st = data.StateInfos[i];
+
+                var stateCell = Instantiate(_cellPref, transform);
+                _cells.Add(stateCell);
+
+                stateCell.TextDescription.text =
+                    $"<color=#FFFF00>{st.StateName}</color> - {st.Description}";
+
+                if (i < pairCnt - 1) stateCell.ShowDividingLine();
+                else stateCell.HideDividingLine();
+            }
+        }
     }
+
 
     public void Hide()
     {
         gameObject.SetActive(false);
-
-        if (_cells == null)
-            return;
-
-        foreach (var cell in _cells)
-            Destroy(cell.gameObject);
-
+        if (_cells == null) return;
+        foreach (var cell in _cells) Destroy(cell.gameObject);
         _cells.Clear();
     }
 }
