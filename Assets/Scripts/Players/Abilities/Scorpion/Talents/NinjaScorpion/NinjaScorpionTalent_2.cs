a56479efbee8d18a5a84eed42f7001b6e71adf20
BrednikFiryn <FirynBrednik@mail.ru>
2025-11-07 20:13:06 +0700

    Brednik07112025_4

--- Assets/Scripts/Players/Abilities/Scorpion/Talents/NinjaScorpion/NinjaScorpionTalent_2.cs
+++ Assets/Scripts/Players/Abilities/Scorpion/Talents/NinjaScorpion/NinjaScorpionTalent_2.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class NinjaScorpionTalent_2 : Talent
 {
     [SerializeField] private ConsumeCombo_Scorpion consume;
+    [SerializeField] SkillManager manager;
 
     public override void Enter()
     {
+        manager.ActivateSkill(consume);
         consume.ConsumeCombo_ScorpionPhysicStateClearTalent(true);
     }
 
     public override void Exit()
     {
+        manager.DeactivateSkill(consume);
         consume.ConsumeCombo_ScorpionPhysicStateClearTalent(false);
     }
 }
