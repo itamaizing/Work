dc87f24aebcac0a2567a1c879b58af04124c0870
BrednikFiryn <FirynBrednik@mail.ru>
2025-12-04 11:19:35 +0700

    Brednik04122025_1

--- Assets/Scripts/Players/Abilities/TerrifyingElf/Shot/Shot/Shot.cs
+++ Assets/Scripts/Players/Abilities/TerrifyingElf/Shot/Shot/Shot.cs
@@ -129,6 +129,7 @@ public class Shot : Skill
             }
             yield return null;
         }
+
         TargetInfo targetInfo = new TargetInfo();
         targetInfo.Targets.Add(target);
         targetInfo.Points.Add(targetPoint);
