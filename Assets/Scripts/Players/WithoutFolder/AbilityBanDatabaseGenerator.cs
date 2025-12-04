ceeb82a4ada983db6b702150c1e19b5e80e9551a
SenX51 <Arsemenov51@yandex.ru>
2025-12-04 15:50:43 +0300

    Fixed editor dependency error when building project
    Added #if UNITY_EDITOR checks for editor-only scripts

--- Assets/Scripts/Players/WithoutFolder/AbilityBanDatabaseGenerator.cs
+++ Assets/Scripts/Players/WithoutFolder/AbilityBanDatabaseGenerator.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 using System;
@@ -34,3 +35,4 @@ public static class AbilityBanDatabaseGenerator
         AssetDatabase.SaveAssets();
     }
 }
+#endif
