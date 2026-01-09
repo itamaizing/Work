da83d15e84f8afde7e27bec03e4ffd5ff9c5a46f
BrednikFiryn <FirynBrednik@mail.ru>
2026-01-09 18:39:20 +0700

    transferring LightningMovement to ReptileTalent_3

--- Assets/Scripts/Players/Abilities/CreeperPoison/Talents/Reptile/ReptileTalent_1.cs
+++ Assets/Scripts/Players/Abilities/CreeperPoison/Talents/Reptile/ReptileTalent_1.cs
@@ -6,17 +6,14 @@ public class ReptileTalent_1 : Talent
 {
     [SerializeField] private SkillManager skillManager;
     [SerializeField] private LightningStrikes lightningStrikes;
-    [SerializeField] private CreeperInvisible creeperInvisible;
 
     public override void Enter()
     {
         skillManager.ActivateSkill(lightningStrikes);
-        skillManager.ActivateSkill(creeperInvisible);
     }
 
     public override void Exit()
     {
         skillManager.DeactivateSkill(lightningStrikes);
-        skillManager.DeactivateSkill(creeperInvisible);
     }
 }
--- Assets/Scripts/Players/Abilities/CreeperPoison/Talents/Reptile/ReptileTalent_3.cs
+++ Assets/Scripts/Players/Abilities/CreeperPoison/Talents/Reptile/ReptileTalent_3.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
 
-public class ReptileTalent_3
+public class ReptileTalent_3 : Talent
 {
-    
+    [SerializeField] private SkillManager skillManager;
+    [SerializeField] private CreeperInvisible creeperInvisible;
+
+    public override void Enter()
+    {
+        skillManager.ActivateSkill(creeperInvisible);
+    }
+
+    public override void Exit()
+    {
+        skillManager.DeactivateSkill(creeperInvisible);
+    }
 }
