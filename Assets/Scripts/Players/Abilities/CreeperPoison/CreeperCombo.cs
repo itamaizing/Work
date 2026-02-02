5a020c97ece824fdb763993827f44ee51fe683f4
BrednikFiryn <FirynBrednik@mail.ru>
2026-02-02 18:41:00 +0700

    working with the creeper combo_1

--- Assets/Scripts/Players/Abilities/CreeperPoison/CreeperCombo.cs
+++ Assets/Scripts/Players/Abilities/CreeperPoison/CreeperCombo.cs
@@ -1,95 +1,167 @@
 using Mirror;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreeperCombo : MonoBehaviour
+public class CreeperCombo : NetworkBehaviour
 {
     [SerializeField] private SneakySpit sneakySpit;
     [SerializeField] private BlockPassiveSkill blockPassiveSkill;
-    [SerializeField] private LightningStrikes _lightningStrikes;
-    [SerializeField] private CreeperStrike _creeperStrike;
     [SerializeField] private SkillManager _skillManager;
 
-    private Queue<Skill> _comboSkillQueue = new();
-    private const int MaxComboSize = 3;
+    private Queue<Skill> _sneakyComboQueue = new();
+    private Queue<Skill> _blockComboQueue = new();
+
+    private Coroutine _resetCoroutine;
+    private float _comboResetDelay = 1f;
+
+    private const int SneakyComboSize = 3;
+    private const int BlockComboSize = 2;
 
     private void OnEnable()
     {
-        _skillManager.SkillCastEnded += RegisterComboSkill;
+        _skillManager.SkillCastEnded += HandleSneakyCombo;
+        _skillManager.SkillCastEnded += HandleBlockCombo;
     }
 
     private void OnDisable()
     {
-        _skillManager.SkillCastEnded -= RegisterComboSkill;
+        _skillManager.SkillCastEnded -= HandleSneakyCombo;
+        _skillManager.SkillCastEnded -= HandleBlockCombo;
     }
 
-    private void RegisterComboSkill(Skill skill)
+    private IEnumerator ResetTimer()
+    {
+        yield return new WaitForSeconds(_comboResetDelay);
+        ClearComboQueue();
+    }
+
+    private void ClearComboQueue()
+    {
+        _sneakyComboQueue.Clear();
+        _blockComboQueue.Clear();
+
+        if (_resetCoroutine != null)
+        {
+            StopCoroutine(_resetCoroutine);
+            _resetCoroutine = null;
+        }
+    }
+
+    private void HandleSneakyCombo(Skill skill)
     {
         if (skill is not CreeperStrike && skill is not LightningStrikes)
         {
-            _comboSkillQueue.Clear();
+            _sneakyComboQueue.Clear();
             return;
         }
 
-        if (_comboSkillQueue.Count >= MaxComboSize)
-            _comboSkillQueue.Dequeue();
+        if (_sneakyComboQueue.Count >= SneakyComboSize)
+            _sneakyComboQueue.Dequeue();
 
-        _comboSkillQueue.Enqueue(skill);
+        if (_resetCoroutine != null) StopCoroutine(_resetCoroutine);
+        _resetCoroutine = StartCoroutine(ResetTimer());
 
-        TryTriggerComboBoost();
+        _sneakyComboQueue.Enqueue(skill);
+        TryTriggerSneakyCombo();
     }
 
-    private void TryTriggerComboBoost()
+    private void TryTriggerSneakyCombo()
     {
-        var combo = _comboSkillQueue.ToArray();
+        var combo = _sneakyComboQueue.ToArray();
+        if (combo.Length < 2) return;
 
-        if (combo.Length == 3 && combo[0] is CreeperStrike && combo[1] is CreeperStrike && combo[2] is CreeperStrike)
+        int creeperCount = 0;
+        int lightningCount = 0;
+
+        foreach (var skill in combo)
         {
-            CmdTriggerSneakySpitFreeWindow(combo[2].GetTargetCharacter());
-            _comboSkillQueue.Clear();
+            if (skill is CreeperStrike) creeperCount++;
+            else if (skill is LightningStrikes) lightningCount++;
         }
 
-        else if (combo.Length >= 2)
+        bool validCombo =
+            lightningCount == 2 ||
+            creeperCount == 3 ||
+            (lightningCount == 1 && creeperCount == 2);
+
+        if (validCombo)
         {
-            if (combo[0] is CreeperStrike && combo[1] is LightningStrikes ||
-                combo[0] is LightningStrikes && combo[1] is CreeperStrike ||
-                combo[0] is LightningStrikes && combo[1] is LightningStrikes)
+            Skill lastSkill = combo[^1];
+            CmdTriggerSneakySpitFreeWindow(lastSkill.GetTargetCharacter());
+            _sneakyComboQueue.Clear();
+
+            if (_resetCoroutine != null)
             {
-                CmdTriggerSneakySpitFreeWindow(combo[1].GetTargetCharacter());
-                _comboSkillQueue.Clear();
+                StopCoroutine(_resetCoroutine);
+                _resetCoroutine = null;
             }
-            else if (combo.Length == 3 &&
-                combo[0] is LightningStrikes &&
-                combo[1] is CreeperStrike &&
-                combo[2] is CreeperStrike)
+        }
+    }
+
+    private void HandleBlockCombo(Skill skill)
+    {
+        if (skill is not CreeperStrike && skill is not LightningStrikes)
+        {
+            _blockComboQueue.Clear();
+            return;
+        }
+
+        if (_blockComboQueue.Count >= BlockComboSize)
+            _blockComboQueue.Dequeue();
+
+        if (_resetCoroutine != null) StopCoroutine(_resetCoroutine);
+        _resetCoroutine = StartCoroutine(ResetTimer());
+
+        _blockComboQueue.Enqueue(skill);
+        TryTriggerBlockCombo();
+    }
+
+    private void TryTriggerBlockCombo()
+    {
+        var combo = _blockComboQueue.ToArray();
+        if (combo.Length < 1) return;
+
+        int creeperCount = 0;
+        int lightningCount = 0;
+
+        foreach (var skill in combo)
+        {
+            if (skill is CreeperStrike) creeperCount++;
+            else if (skill is LightningStrikes) lightningCount++;
+        }
+
+        bool validCombo =
+            lightningCount == 1 ||
+            creeperCount == 2 ||
+            (creeperCount == 1 && lightningCount == 1);
+
+        if (validCombo)
+        {
+            Skill lastSkill = combo[^1];
+            CmdBlockPassiveSkillFreeWindow(lastSkill.GetTargetCharacter());
+            _blockComboQueue.Clear();
+
+            if (_resetCoroutine != null)
             {
-                CmdTriggerSneakySpitFreeWindow(combo[1].GetTargetCharacter());
-                _comboSkillQueue.Clear();
+                StopCoroutine(_resetCoroutine);
+                _resetCoroutine = null;
             }
         }
     }
 
     [Command] private void CmdTriggerSneakySpitFreeWindow(Character target) => RpcTriggerSneakySpitWindow(target);
-
-    [Command] private void CmdTriggerSneakySpitWindowCancel() => RpcTriggerSneakySpitWindowCancel();
-
     [Command] private void CmdBlockPassiveSkillFreeWindow(Character target) => RpcBlockPassiveSkillFreeWindow(target);
 
     [ClientRpc]
     private void RpcTriggerSneakySpitWindow(Character target)
     {
-        if (sneakySpit != null) sneakySpit.TryStartSneakySpitBoostWindow(target);
-    }
-
-    [ClientRpc]
-    private void RpcTriggerSneakySpitWindowCancel()
-    {
-        if (sneakySpit != null) sneakySpit.CancelBoostWindow();
+        sneakySpit?.TryStartSneakySpitBoostWindow(target);
     }
 
     [ClientRpc]
     private void RpcBlockPassiveSkillFreeWindow(Character target)
     {
-        if (blockPassiveSkill != null) blockPassiveSkill.TryStartBlockPassiveSkillBoostWindow(target);
+        blockPassiveSkill?.TryStartBlockPassiveSkillBoostWindow(target);
     }
 }
