27941ab716e91aa44d859819bc344c25d4ac76a1
BrednikFiryn <FirynBrednik@mail.ru>
2026-02-26 13:19:10 +0700

    fix Spisnacider attack

---
+++ Assets/Scripts/Players/Abilities/Spisnacider/SpisnaciderAutoAttack.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SpisnaciderAutoAttack : MonoBehaviour
+{
+    [SerializeField] private SpellMoveSpisnaciderTo _spellMoveSpisnacider;
+    [SerializeField] private SpittingAcid _spittingAcid;
+
+    public void OnAutoAttackAnimationHitSpisnacider() => _spellMoveSpisnacider.OnAutoAttackAnimationHit();
+    public void OnAutoAttackAnimationEndSpisnacider() => _spellMoveSpisnacider.OnAutoAttackAnimationEnd();
+
+    public void ScratchClawsAttackAnimationHit() => _spittingAcid.AttackAnimationHit();
+}
