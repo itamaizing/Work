155114c0eb7fec1e533f4bbab48bac2f1ff1196e
BrednikFiryn <FirynBrednik@mail.ru>
2026-03-03 10:29:45 +0700

    inheriting creature attacks from a common parent

--- Assets/Scripts/Players/Abilities/CarryGun/Creature/SkillCreatureCarryGun.cs
+++ Assets/Scripts/Players/Abilities/CarryGun/Creature/SkillCreatureCarryGun.cs
@@ -1,6 +1,207 @@
+using DG.Tweening;
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class SkillCreature
+public abstract class SkillCreatureCarryGun : Skill
 {
-    
+    [Header("Common Settings")]
+    [SerializeField] protected Animator animator;
+    [SerializeField] protected float moveDurationPerUnit = 0.2f;
+    [SerializeField] protected float stopDistance = 1.5f;
+    [SerializeField] protected float targetSearchRadius = 0.5f;
+
+    protected IDamageable currentTarget;
+    protected Tween activeTween;
+    protected Coroutine moveCoroutine;
+    protected bool moveActive;
+
+    protected const float StopDistanceThreshold = 0.05f;
+    protected const float SegmentMinDistance = 0.01f;
+    protected const float RaycastCheckDistance = 1f;
+    protected const float MoveEventThreshold = 1f;
+
+    public Action<GameObject> DoMove;
+
+    protected abstract string AnimationTrigger { get; }
+    protected abstract void ApplySkillEffect(Character target);
+
+    protected override int AnimTriggerCastDelay => 0;
+    protected override int AnimTriggerCast => 0;
+
+    protected override bool IsCanCast => GetTarget() != null;
+
+    private bool IsAllyTarget(IDamageable target)
+        => target.gameObject.layer == LayerMask.NameToLayer("Allies");
+
+    #region Target
+
+    public override void LoadTargetData(TargetInfo targetInfo)
+    {
+        if (targetInfo.GetTargets().Count > 0)
+            SetTarget(targetInfo.GetTargets()[0]);
+    }
+
+    protected override IEnumerator PrepareJob(Action<TargetInfo> callback)
+    {
+        while (GetTempTarget() == null)
+        {
+            if (GetMouseButton)
+            {
+                FindTarget(targetSearchRadius, GetMousePoint());
+
+                if (GetTempTarget() is IDamageable dmg)
+                {
+                    if (IsAllyTarget(dmg) || dmg as Character == Hero)
+                        ClearTempTarget();
+                    else
+                        break;
+                }
+            }
+
+            yield return null;
+        }
+
+        SetTarget(GetTempTarget());
+
+        TargetInfo info = new();
+        info.AddTarget(GetTarget());
+        callback?.Invoke(info);
+    }
+
+    #endregion
+
+    #region Cast
+
+    protected override IEnumerator CastJob()
+    {
+        CancelWork();
+        moveActive = true;
+        currentTarget = GetTarget() as Character;
+
+        float distance = Vector3.Distance(transform.position, currentTarget.transform.position);
+
+        if (distance > stopDistance + StopDistanceThreshold)
+        {
+            moveCoroutine = StartCoroutine(MoveToTarget(currentTarget));
+            while (moveActive) yield return null;
+        }
+        else
+        {
+            TriggerAnimation();
+            while (moveActive) yield return null;
+        }
+    }
+
+    protected void TriggerAnimation()
+    {
+        animator.SetTrigger(AnimationTrigger);
+    }
+
+    public void AnimationHit()
+    {
+        if (currentTarget is Character ch)
+            ApplySkillEffect(ch);
+
+        moveActive = false;
+    }
+
+    #endregion
+
+    #region Movement
+
+    private IEnumerator MoveToTarget(IDamageable target)
+    {
+        if (target == null) yield break;
+
+        Hero.Move.LookAtPosition(target.transform.position);
+
+        Vector3 destination = GetApproachPoint(target);
+        Hero.Move.SetCanMove(false);
+
+        NavMeshPath path = new();
+        bool hasPath = NavMesh.CalculatePath(transform.position, destination, NavMesh.AllAreas, path);
+
+        if (!hasPath || path.status != NavMeshPathStatus.PathComplete)
+        {
+            Hero.Move.SetCanMove(true);
+            yield break;
+        }
+
+        Vector3 lastMovePoint = transform.position;
+
+        for (int i = 1; i < path.corners.Length; i++)
+        {
+            Vector3 segment = path.corners[i];
+            float dist = Vector3.Distance(transform.position, segment);
+            float duration = dist * moveDurationPerUnit;
+
+            if (dist < SegmentMinDistance) continue;
+
+            bool interrupted = false;
+
+            activeTween?.Kill();
+
+            activeTween = transform.DOMove(segment, duration)
+                .SetEase(Ease.Linear)
+                .OnUpdate(() =>
+                {
+                    float moved = Vector3.Distance(lastMovePoint, transform.position);
+                    if (moved >= MoveEventThreshold)
+                    {
+                        DoMove?.Invoke(gameObject);
+                        lastMovePoint = transform.position;
+                    }
+
+                    if (Physics.Raycast(transform.position, transform.forward, RaycastCheckDistance, _obstacle))
+                        interrupted = true;
+
+                    if (interrupted)
+                        activeTween?.Kill();
+                });
+
+            yield return activeTween.WaitForCompletion();
+            if (interrupted) break;
+        }
+
+        Hero.Move.SetCanMove(true);
+        TriggerAnimation();
+    }
+
+    private Vector3 GetApproachPoint(IDamageable enemy)
+    {
+        Vector3 dir = (enemy.transform.position - transform.position).normalized;
+        return enemy.transform.position - dir * stopDistance;
+    }
+
+    #endregion
+
+    #region Cleanup
+
+    protected override void ClearData()
+    {
+        ClearTarget();
+        ClearTempTarget();
+        currentTarget = null;
+
+        Hero?.Move?.SetCanMove(true);
+        Hero?.Move?.StopLookAt();
+
+        moveActive = false;
+        CancelWork();
+    }
+
+    private void CancelWork()
+    {
+        activeTween?.Kill();
+
+        if (moveCoroutine != null)
+        {
+            StopCoroutine(moveCoroutine);
+            moveCoroutine = null;
+        }
+    }
+
+    #endregion
 }
--- Assets/Scripts/Players/Abilities/Getomir/PowerStrike.cs
+++ Assets/Scripts/Players/Abilities/Getomir/PowerStrike.cs
@@ -1,282 +1,36 @@
-using DG.Tweening;
-using System;
-using System.Collections;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class PowerStrike : Skill
+public class PowerStrike : SkillCreatureCarryGun
 {
-    [SerializeField] private Animator _animator;
-    [SerializeField] private Character _playerLinks;
-    [SerializeField] private float _moveDurationPerUnit = 0.2f;
-    [SerializeField] private float _stopDistance = 1.5f;
-    [SerializeField] private float _minDamage = 12f;
-    [SerializeField] private float _maxDamage = 18f;
-    [SerializeField] private float _aoeRadius = 1.5f;
+    [SerializeField] private float minDamage = 12f;
+    [SerializeField] private float maxDamage = 18f;
+    [SerializeField] private float aoeRadius = 1.5f;
 
-    [SerializeField] private CreatureCarryGun _creatureCarryGun;
+    protected override string AnimationTrigger => "AttackGetomir";
 
-    #region Const
-    private const float StopDistanceThreshold = 0.05f;
-    private const float MoveEventThreshold = 1f;
-    private const float SegmentMinDistance = 0.01f;
-    private const float RaycastCheckDistance = 1f;
-    private const float TargetSearchRadius = 0.5f;
-
-    private const string AttackGetomirTrigger = "AttackGetomir";
-    #endregion
-
-    private IDamageable _currentTarget;
-    private Tween _activeTween;
-    private Coroutine _moveCoroutine;
-    private bool _moveActive = false;
-
-    public Action<GameObject> DoMove;
-
-    protected override int AnimTriggerCastDelay => 0;
-    protected override int AnimTriggerCast => 0;
-
-    private void powerStrikeAnimCast()
+    protected override void ApplySkillEffect(Character mainTarget)
     {
-        _animator.SetTrigger(AttackGetomirTrigger);
-    }
+        float baseDamage = Buff.Damage.GetBuffedValue(
+            UnityEngine.Random.Range(minDamage, maxDamage));
 
-    public void AttackAnimationHit()
-    {
-        ApplyStrikeDamage();
-        _moveActive = false;
-    }
-
-    protected override bool IsCanCast => GetTarget() != null;
-    private bool IsAllyTarget(IDamageable target) => target.gameObject.layer == LayerMask.NameToLayer("Allies");
-
-    public override void LoadTargetData(TargetInfo targetInfo)
-    {
-        if (targetInfo.GetTargets().Count > 0) SetTarget(targetInfo.GetTargets()[0]);
-    }
-
-    private void OnEnable()
-    {
-        Damage = UnityEngine.Random.Range(_minDamage, _maxDamage);
-        OnSkillCanceled += HandleSkillCanceled;
-    }
-
-    private void OnDisable()
-    {
-        OnSkillCanceled -= HandleSkillCanceled;
-    }
-
-    private void HandleSkillCanceled()
-    {
-        if (_hero?.Move != null)
-        {
-            Hero.Move.SetCanMove(true);
-            Hero.Move.StopLookAt();
-        }
-
-        _currentTarget = null;
-        CancelWork();
-
-        _moveActive = false;
-        ClearTarget();
-        ClearTempTarget();
-    }
-
-    protected override IEnumerator PrepareJob(Action<TargetInfo> targetDataSavedCallback)
-    {
-        while (GetTempTarget() == null)
-        {
-            if (GetMouseButton)
-            {
-                FindTarget(TargetSearchRadius, GetMousePoint());
-
-                if (GetTempTarget() != null && GetTempTarget() is IDamageable damageable)
-                {
-                    if (IsAllyTarget(damageable) || damageable as Character == Hero) ClearTempTarget();
-                    else break;
-                }
-            }
-
-            yield return null;
-        }
-
-        SetTarget(GetTempTarget());
-
-        TargetInfo info = new();
-        info.AddTarget(GetTarget());
-        targetDataSavedCallback?.Invoke(info);
-    }
-
-    protected override IEnumerator CastJob()
-    {
-        CancelWork();
-        _moveActive = true;
-        _currentTarget = GetTarget() as Character;
-
-        float distanceToTarget = Vector3.Distance(transform.position, _currentTarget.transform.position);
-        if (distanceToTarget > _stopDistance + StopDistanceThreshold)
-        {
-            if (_moveCoroutine != null)
-            {
-                StopCoroutine(_moveCoroutine);
-                _moveCoroutine = null;
-            }
-
-            _moveCoroutine = StartCoroutine(MoveToTargetCharacter(_currentTarget));
-            while (_moveActive) yield return null;
-        }
-
-        else
-        {
-            powerStrikeAnimCast();
-            while (_moveActive) yield return null;
-        }
-    }
-
-    protected override void ClearData()
-    {
-        ClearTarget();
-        ClearTempTarget();
-        _currentTarget = null;
-
-        if (_hero?.Move != null)
-        {
-            Hero.Move.SetCanMove(true);
-            Hero.Move.StopLookAt();
-        }
-
-        _moveActive = false;
-
-        CancelWork();
-    }
-
-    private IEnumerator MoveToTargetCharacter(IDamageable target)
-    {
-        if (target == null) yield break;
-
-        Hero.Move.LookAtPosition(target.transform.position);
-
-        Vector3 destination = GetApproachPointNearEnemy(target);
-
-        Hero.Move.SetCanMove(false);
-
-        NavMeshPath path = new NavMeshPath();
-
-        bool hasPath = NavMesh.CalculatePath(transform.position, destination, NavMesh.AllAreas, path);
-
-        if (!hasPath || path.status != NavMeshPathStatus.PathComplete)
-        {
-            Hero.Move.SetCanMove(true);
-            yield break;
-        }
-
-        Vector3 lastDoMovePoint = transform.position;
-
-        for (int i = 1; i < path.corners.Length; i++)
-        {
-            Vector3 segmentTarget = path.corners[i];
-            float distance = Vector3.Distance(transform.position, segmentTarget);
-            float duration = distance * _moveDurationPerUnit;
-
-            if (distance < SegmentMinDistance) continue;
-
-            bool interrupted = false;
-
-            if (_activeTween != null && _activeTween.IsActive())
-            {
-                _activeTween.Kill();
-                _activeTween = null;
-            }
-
-            _activeTween = transform.DOMove(segmentTarget, duration)
-                .SetEase(Ease.Linear)
-                .OnUpdate(() =>
-                {
-                    if (!gameObject.activeInHierarchy) return;
-
-                    float movedDist = Vector3.Distance(lastDoMovePoint, transform.position);
-                    if (movedDist >= MoveEventThreshold)
-                    {
-                        DoMove?.Invoke(gameObject);
-                        lastDoMovePoint = transform.position;
-                    }
-
-                    if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, RaycastCheckDistance, _obstacle)) interrupted = true;
-
-                    if (interrupted)
-                    {
-                        if (_activeTween != null && _activeTween.IsActive()) _activeTween.Kill();
-                    }
-                });
-
-            yield return _activeTween.WaitForCompletion();
-            if (interrupted) break;
-        }
-
-        Hero.Move.SetCanMove(true);
-
-        powerStrikeAnimCast();
-    }
-
-    private Vector3 GetApproachPointNearEnemy(IDamageable enemy)
-    {
-        Vector3 toEnemy = (enemy.transform.position - transform.position).normalized;
-        return enemy.transform.position - toEnemy * _stopDistance;
-    }
-
-    private void ApplyStrikeDamage()
-    {
-        if (_currentTarget == null) return;
-
-        Character mainTarget = _currentTarget as Character;
-        if (mainTarget == null) return;
-
-        float randomDamage = UnityEngine.Random.Range(_minDamage, _maxDamage);
-        float baseDamage = Buff.Damage.GetBuffedValue(randomDamage);
-
-        Vector3 center = mainTarget.transform.position;
-
-        Collider[] hits = Physics.OverlapSphere(center, _aoeRadius, TargetsLayers);
+        Collider[] hits = Physics.OverlapSphere(mainTarget.transform.position, aoeRadius, TargetsLayers);
 
         foreach (var hit in hits)
         {
             Character character = hit.GetComponent<Character>();
-            if (character == null) continue;
-            if (!IsValidTarget(character)) continue;
+            if (character == null || character == Hero || character.IsDead)
+                continue;
 
-            float finalDamage = character == mainTarget ? baseDamage : baseDamage * 0.5f;
+            float final = character == mainTarget ? baseDamage : baseDamage * 0.5f;
 
             Damage damage = new Damage
             {
-                Value = finalDamage,
+                Value = final,
                 Type = DamageType,
                 PhysicAttackType = AttackRangeType
             };
 
             CmdApplyDamage(damage, character.gameObject);
-        }
-    }
-
-    private bool IsValidTarget(Character character)
-    {
-        if (character == Hero) return false;
-        if (character.IsDead) return false;
-
-        return true;
-    }
-
-    private void CancelWork()
-    {
-        if (_activeTween != null && _activeTween.IsActive())
-        {
-            _activeTween.Kill();
-            _activeTween = null;
-        }
-
-        if (_moveCoroutine != null)
-        {
-            StopCoroutine(_moveCoroutine);
-            _moveCoroutine = null;
         }
     }
 }
