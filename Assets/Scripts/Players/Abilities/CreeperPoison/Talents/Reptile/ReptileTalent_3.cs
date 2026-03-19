f88789c5d2c21cf11f49779f5b60f2bbb7695ecf
BrednikFiryn <FirynBrednik@mail.ru>
2026-03-19 23:26:10 +0700

    talent poison cloud

--- Assets/Scripts/Players/Abilities/CreeperPoison/Talents/Reptile/ReptileTalent_3.cs
+++ Assets/Scripts/Players/Abilities/CreeperPoison/Talents/Reptile/ReptileTalent_3.cs
@@ -4,14 +4,23 @@ public class ReptileTalent_3 : Talent
 {
     [SerializeField] private SkillManager skillManager;
     [SerializeField] private CreeperInvisible creeperInvisible;
+    [SerializeField] private SpeedOfReptile speed;
+    [SerializeField] private MetabolismReptile _metabolismReptile;
+    [SerializeField] private PoisonSlap _poisonSlap;
 
     public override void Enter()
     {
         skillManager.ActivateSkill(creeperInvisible);
+        skillManager.ActivateSkill(speed);
+        skillManager.ActivateSkill(_metabolismReptile);
+        skillManager.ActivateSkill(_poisonSlap);
     }
 
     public override void Exit()
     {
         skillManager.DeactivateSkill(creeperInvisible);
+        skillManager.DeactivateSkill(speed);
+        skillManager.DeactivateSkill(_metabolismReptile);
+        skillManager.DeactivateSkill(_poisonSlap);
     }
 }
