454ba94fb10fd393f87687ff70014b7b61346c29
BrednikFiryn <FirynBrednik@mail.ru>
2026-03-31 19:06:29 +0700

    fix

---
+++ Assets/Scripts/Players/Abilities/CreeperPoison/Talents/NinjaPoison/NinjaPoisonTalent_10.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaPoisonTalent_10 : Talent
+{
+    public override void Enter()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
