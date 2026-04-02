5146572e016f3748a15920bddde3f796b9a77e5d
BrednikFiryn <FirynBrednik@mail.ru>
2026-04-02 12:29:25 +0700

    GrabTongue

--- Assets/Scripts/Players/Abilities/CreeperPoison/Talents/Reptile/ReptileTalent_2.cs
+++ Assets/Scripts/Players/Abilities/CreeperPoison/Talents/Reptile/ReptileTalent_2.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class ReptileTalent_2 : Talent
 {
+    [SerializeField] private GrabTongue _grabTongue;
+    [SerializeField] private SkillManager _skillManager;
+
     public override void Enter()
     {
-
+        _skillManager.ActivateSkill(_grabTongue);
     }
 
     public override void Exit()
     {
-
+        _skillManager.DeactivateSkill(_grabTongue);
     }
 }
