4dd8999d309c069f2de82d39bfaf802816240a60
BrednikFiryn <FirynBrednik@mail.ru>
2026-04-13 13:19:21 +0700

    Talent transfer

--- Assets/Scripts/Players/Abilities/TerrifyingElf/Talents/Darkness/DarknessTalent_10.cs
+++ Assets/Scripts/Players/Abilities/TerrifyingElf/Talents/Darkness/DarknessTalent_10.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class DarknessTalent_10 : Talent
 {
     [SerializeField] private Silence silence;
+    [SerializeField] private TerrifyingElfAura terrifyingElfAura;
 
     public override void Enter()
     {
         silence.SilenceAddAllCharacterWithDeabaffElf(true);
+        terrifyingElfAura.SuppressionManaAbsorption(true);
     }
 
     public override void Exit()
     {
         silence.SilenceAddAllCharacterWithDeabaffElf(false);
+        terrifyingElfAura.SuppressionManaAbsorption(false);
     }
 }
--- Assets/Scripts/Players/Abilities/TerrifyingElf/Talents/Darkness/DarknessTalent_14.cs
+++ Assets/Scripts/Players/Abilities/TerrifyingElf/Talents/Darkness/DarknessTalent_14.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class DarknessTalent_14 : Talent
 {
     [SerializeField] private Ghost ghost;
+    [SerializeField] private PullingHealth pullingHealth;
+    [SerializeField] private Silence silence;
 
     public override void Enter()
     {
         ghost.CooldownGhostShotActiveTalent(true);
+        pullingHealth.SetPullingHealthGhostTalentActive(true);
+        silence.SilenceEffectsOnMinionMagic(true);
+        silence.GhostDeathSilence(true);
     }
 
     public override void Exit()
     {
         ghost.CooldownGhostShotActiveTalent(false);
+        pullingHealth.SetPullingHealthGhostTalentActive(false);
+        silence.SilenceEffectsOnMinionMagic(false);
+        silence.GhostDeathSilence(false);
     }
 }
