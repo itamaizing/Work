2109c46ed231fc1b3643bc76b766476b72a5abe4
BrednikFiryn <FirynBrednik@mail.ru>
2026-04-13 12:10:55 +0700

    changing the TeamIndex when entering sleep

--- Assets/Scripts/Players/Abilities/TerrifyingElf/Talents/Nature/NatureTalent_1.cs
+++ Assets/Scripts/Players/Abilities/TerrifyingElf/Talents/Nature/NatureTalent_1.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class NatureTalent_1 : Talent
 {
+	[SerializeField] private SleepSpell sleep;
+	[SerializeField] private SkillManager _ability;
+
 	public override void Enter()
 	{
-
+		_ability.ActivateSkill(sleep);
 	}
 
 	public override void Exit()
 	{
-
+		_ability.DeactivateSkill(sleep);
 	}
 }
