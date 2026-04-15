af93d70a26a4fb1010b628241dffb42978050d44
BrednikFiryn <FirynBrednik@mail.ru>
2026-04-16 00:08:47 +0700

    IceDeath Talent Transfer

--- Assets/Scripts/Players/Abilities/IceDeath/Talents/Ninja/NinjaTalent_4.cs
+++ Assets/Scripts/Players/Abilities/IceDeath/Talents/Ninja/NinjaTalent_4.cs
@@ -7,15 +7,19 @@ public class NinjaTalent_4 : Talent
     [SerializeField] private IceShadow iceShadow;
     [SerializeField] private IcePuddle icePuddle;
 
+    [SerializeField] private SeriesOfStrikes _seriesOfStrikes;
+
     public override void Enter()
     {
         iceShadow.IceDeathInShadowTalentActive(true, Data.DescriptionsForInfoPanel[0]);
+        _seriesOfStrikes.SeriesCompliteCompoTalentActive(true);
         //icePuddle.IceDeathInIcePudleTalentActive(true, "");
     }
 
     public override void Exit()
     {
         iceShadow.IceDeathInShadowTalentActive(false, Data.DescriptionsForInfoPanel[0]);
+        _seriesOfStrikes.SeriesCompliteCompoTalentActive(false);
         //icePuddle.IceDeathInIcePudleTalentActive(false, "");
     }
 }
