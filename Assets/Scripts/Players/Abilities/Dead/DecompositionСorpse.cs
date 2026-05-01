5bb7bfe53f0bf876951250cfda9484e9624f17f7
BrednikFiryn <FirynBrednik@mail.ru>
2026-05-01 15:37:32 +0700

    Creating a plague cloud when destroying a corpse

--- Assets/Scripts/Players/Abilities/Dead/DecompositionСorpse.cs
+++ Assets/Scripts/Players/Abilities/Dead/DecompositionСorpse.cs
@@ -4,6 +4,8 @@ using System.Collections;
 
 public class DecompositionCorpse : NetworkBehaviour
 {
+    [SerializeField] private GameObject _plagueCloudPrefab;
+
     private Health _health;
     private Coroutine _decompositionRoutine;
 
@@ -44,10 +46,20 @@ public class DecompositionCorpse : NetworkBehaviour
         }
 
         if (_health != null) _health.Died -= OnDied;
+
+        SpawnCloud(transform.position);
     }
 
     private void OnDestroy()
     {
         if (_health != null) _health.Died -= OnDied;
+    }
+
+    [Command]
+    private void SpawnCloud(Vector3 position)
+    {
+        GameObject cloud = Instantiate(_plagueCloudPrefab, position, Quaternion.identity);
+
+        NetworkServer.Spawn(cloud);
     }
 }
