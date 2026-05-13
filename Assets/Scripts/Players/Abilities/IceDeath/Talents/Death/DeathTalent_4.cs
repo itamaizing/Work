dc4b2367a6a132b3311676b762d112e8baed3649
BrednikFiryn <FirynBrednik@mail.ru>
2026-05-13 20:36:57 +0700

    fix Skill panel

---
+++ Assets/Scripts/Players/Abilities/IceDeath/Talents/Death/DeathTalent_4.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTalent_4 : Talent
+{
+	[SerializeField] private DeathSpiral _deathSpiral;
+	[SerializeField] private SkillManager _ability;
+	public override void Enter()
+	{
+		_ability.ActivateSkill(_deathSpiral);
+	}
+
+	public override void Exit()
+	{
+		_ability.DeactivateSkill(_deathSpiral);
+	}
+}
