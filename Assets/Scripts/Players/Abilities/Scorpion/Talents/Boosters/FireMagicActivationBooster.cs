19ec8b6549333b4ea20c236a9b6680cdffe90ad5
SenX51 <Arsemenov51@yandex.ru>
2026-07-15 23:37:14 +0300

    Small fixes
    > Поправил о6:
       - 4 дублирующихся метода для подписки -> 1 handler, по аналогии с используемся здесь же.
       - Огненные скиллы смотрят не на CastStart, а на CastSuccess
    
    > Поправил затраты маны у FireBreath (могу быть не прав)

--- Assets/Scripts/Players/Abilities/Scorpion/Talents/Boosters/FireMagicActivationBooster.cs
+++ Assets/Scripts/Players/Abilities/Scorpion/Talents/Boosters/FireMagicActivationBooster.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
@@ -6,14 +6,11 @@ using UnityEngine;
 
 public class FireMagicActivationBooster : Skill, IPassiveSkill
 {
-    private const float SkillCooldown = 6f;
-
-    private NewPunch_Scorpion _punch;
-    private Kick_Scorpion _kick;
-    private CleavingBlade_Scorpion _cleavingBlade;
-    private ChainBlade _chainBlade;
+    [SerializeField] private float SkillCooldown = 6f;
 
     private readonly Dictionary<Skill, System.Action> _fireMagicHandlers = new();
+    private readonly Dictionary<Skill, System.Action> _consumeHandlers = new();
+    private readonly Dictionary<Skill, float> _cooldowns = new();
 
     private float _chargeConsumedAt = float.NegativeInfinity;
     private bool  _isEnabled;
@@ -23,39 +20,33 @@ public class FireMagicActivationBooster : Skill, IPassiveSkill
         if (_isEnabled == value) return;
         _isEnabled = value;
 
-        _punch = _hero.Abilities.GetSkill<NewPunch_Scorpion>();
-        _kick = _hero.Abilities.GetSkill<Kick_Scorpion>();
-        _cleavingBlade = _hero.Abilities.GetSkill<CleavingBlade_Scorpion>();
-        _chainBlade = _hero.Abilities.GetSkill<ChainBlade>();
-
         if (value)
         {
             foreach (var skill in _hero.Abilities.Abilities)
             {
-                if (skill.Info.School != Schools.Fire) continue;
-                if (skill is NewPunch_Scorpion or Kick_Scorpion or CleavingBlade_Scorpion) continue;
-
-                var captured = skill;
-                System.Action handler = () => OnFireMagicCastStarted(captured);
-                _fireMagicHandlers[captured] = handler;
-                captured.CastStarted += handler;
+                if (skill is NewPunch_Scorpion or Kick_Scorpion or CleavingBlade_Scorpion or ChainBlade)
+                {
+                    System.Action handler = () => OnConsumeSkillStarted(skill);
+                    _consumeHandlers[skill] = handler;
+                    skill.CastStarted += handler;
+                }
+                else if (skill.Info.School == Schools.Fire)
+                {
+                    System.Action handler = () => OnFireMagicCastStarted(skill);
+                    _fireMagicHandlers[skill] = handler;
+                    skill.CastSuccess += handler;
+                }
             }
-
-            _punch.CastStarted += OnPunchCastStarted;
-            _kick.CastStarted += OnKickCastStarted;
-            _cleavingBlade.CastStarted += OnBladeCastStarted;
-            _chainBlade.OnArrowHit += OnChainBladeHit;
         }
         else
         {
             foreach (var kvp in _fireMagicHandlers)
-                kvp.Key.CastStarted -= kvp.Value;
+                kvp.Key.CastSuccess -= kvp.Value;
             _fireMagicHandlers.Clear();
 
-            _punch.CastStarted -= OnPunchCastStarted;
-            _kick.CastStarted -= OnKickCastStarted;
-            _cleavingBlade.CastStarted -= OnBladeCastStarted;
-            _chainBlade.OnArrowHit -= OnChainBladeHit;
+            foreach (var kvp in _consumeHandlers)
+                kvp.Key.CastStarted -= kvp.Value;
+            _consumeHandlers.Clear();
 
             if (_hero.isClient)
                 _hero.CharacterState.CmdRemoveState(States.FireCharge);
@@ -64,54 +55,38 @@ public class FireMagicActivationBooster : Skill, IPassiveSkill
 
     private void OnFireMagicCastStarted(Skill skill)
     {
-        if (Time.time - _chargeConsumedAt < SkillCooldown) return;
-        
         if (_hero.CharacterState.GetState(States.FireCharge) != null) return;
+        if (_cooldowns.TryGetValue(skill, out var cooldown) && Time.time < cooldown) return;
 
         if (_hero.isClient)
             _hero.CharacterState.CmdAddState(
                 States.FireCharge, float.PositiveInfinity, 0f,
                 Schools.Fire, _hero.gameObject, null);
+        _cooldowns[skill] = Time.time + SkillCooldown;
     }
 
-    private void OnPunchCastStarted()
+    private void OnConsumeSkillStarted(Skill skill)
     {
         var state = _hero.CharacterState.GetState(States.FireCharge) as FireChargeState;
         if (state == null) return;
 
-        state.ConsumeForPunchKick(punch: _punch);
-        RecordConsumption();
-    }
-
-    private void OnKickCastStarted()
-    {
-        var state = _hero.CharacterState.GetState(States.FireCharge) as FireChargeState;
-        if (state == null) return;
-
-        state.ConsumeForPunchKick(kick: _kick);
-        RecordConsumption();
-    }
-
-    private void OnBladeCastStarted()
-    {
-        var state = _hero.CharacterState.GetState(States.FireCharge) as FireChargeState;
-        if (state == null) return;
-
-        state.ConsumeForBlades(blade: _cleavingBlade);
-        RecordConsumption();
-    }
-
-    private void OnChainBladeHit(Character target)
-    {
-        var state = _hero.CharacterState.GetState(States.FireCharge) as FireChargeState;
-        if (state == null) return;
-
-        state.ConsumeForBlades(chainBlade: _chainBlade);
-        RecordConsumption();
+        switch (skill)
+        {
+            case NewPunch_Scorpion punch:
+                state.ConsumeForPunchKick(punch: punch);
+                break;
+            case Kick_Scorpion kick:
+                state.ConsumeForPunchKick(kick: kick);
+                break;
+            case CleavingBlade_Scorpion _cleavingBlade:
+                state.ConsumeForBlades(blade: _cleavingBlade);
+                break;
+            case ChainBlade _chainBlade:
+                state.ConsumeForBlades(chainBlade: _chainBlade);
+                break;
+        }
     }
     
-    private void RecordConsumption() => _chargeConsumedAt = Time.time;
-
     protected override IEnumerator CastJob()
     {
         throw new System.NotImplementedException();
